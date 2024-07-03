e0f20bea4412eb017553b10e3ee5985d265577d0
Ben <ben.d.godfrey2002@gmail.com>
2024-07-03 22:36:18 +0100

    Completed PE5

---
+++ PE5.py
@@ -0,0 +1,26 @@
+# https://projecteuler.net/problem=5
+
+# smallest positive integer divisible by all 1, ..., 20
+# no computation required. Find all prime factors requried
+
+# 2 = 2
+# 3 = 3
+# 4 = 2,2
+# 5 = 5             1 five
+# 6 = 3,2
+# 7 = 7             1 seven
+# 8 = 2,2,2
+# 9 = 3,3
+# 10 = 2,5
+# 11 = 11           1 eleven
+# 12 = 2,2,3
+# 13 = 13           1 thirteen
+# 14 = 2,7
+# 15 = 3,5
+# 16 = 2,2,2,2      4 twos
+# 17 = 17           1 seventeen
+# 18 = 2,3,3        2 threes
+# 19 = 19           1 nineteen
+# 20 = 2,2,5
+
+print(5 * 7 * 11 * 13 * 2 * 2 * 2 * 2 * 17 * 3 * 3 * 19)
